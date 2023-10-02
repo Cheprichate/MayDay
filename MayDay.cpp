e43a28e6a6b27a9efd9f6263d30200161eea752c
Cheprichate <petrovich15071998@mail.ru>
2023-10-02 15:59:26 +0400

    Добавьте файлы проекта.

---
+++ MayDay.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+int week;
+int day;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Какой сегодня день ? \n";
+    std::cin >> day;
+    std::cout << "С какого дня начался месяц ? \n";
+    std::cin >> week;
+
+    if (day <= 0 || day > 31 || week > 7) {
+    std::cout << "Введены некорректные значения . \n";
+    }
+    else if (day >= 1 && day <= 5 || day >= 8 && day <= 10) {
+        std::cout << "Сегодня выходной праздничный день \n";
+    }
+    else if ((day + week) % 7 <= 1) {
+        std::cout << "Сегодня выходной день \n";
+        }
+    else {
+    std::cout << "Сегодня рабочий день \n";
+}
+}
